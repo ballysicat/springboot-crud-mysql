09ecf6a2cc01ba9fc6c2b211db52fb0360fa48b5
BallySicat <88377731+ballysicat@users.noreply.github.com>
2025-01-28 09:05:37 +0800

    Update Api.java

--- src/main/java/com/example/mysql/controller/Api.java
+++ src/main/java/com/example/mysql/controller/Api.java
@@ -15,31 +15,37 @@ import com.example.mysql.services.UserService;
 
 @RestController
 public class Api {
+    // Inject the service class
     @Autowired
     private UserService userservice;
 
-
+    // Default endpoint (Index)
     @GetMapping("/")
     public List<User> getuser(){
         return userservice.getall();
     }
 
+    // Endpoint with ID path variable
     @GetMapping("/{id}")
     @ResponseBody
     public User getbyid(@PathVariable("id") Long id){
         return userservice.getbyid(id);
     }
 
+    // Endpoint with create function or method
     @PostMapping("/create")
     public User create(@RequestBody User newuser){
          return userservice.createUser(newuser); 
     }
+
+     // Endpoint with delete function or method and ID
     @DeleteMapping("/delete/{id}")
     public String delete(@PathVariable("id") Long id){
          userservice.deleteuser(id);
          return "Deleted";
     }
 
+    // Endpoint with update function or method and ID
     @PostMapping("/update/{id}")
     public String update(@PathVariable("id") Long entity) {        
         userservice.updateuser(null);
